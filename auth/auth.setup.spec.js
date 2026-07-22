ec5791256779628e2d21e78cc9b350bb6b52b407
Siva Prakash <psivaprakash38@gmail.com>
2026-07-23 03:18:42 +0530

    new updated

--- auth/auth.setup.spec.js
+++ auth/auth.setup.spec.js
@@ -6,7 +6,7 @@ test('Authenticate User', async ({ browser }) => {
     const context = await browser.newContext()
     const page = await context.newPage()
     const loginPage = new SignIn(page)
-    await loginPage.browserLaunch(userLoginData)
+    await loginPage.browserLaunch()
     await loginPage.userLogin(userLoginData)
     await expect(page).toHaveURL(/.*\/dashboard\/index$/)
     await context.storageState({ path: 'playwright/.auth/user.json' })
