e93410e4bd09525714d8dce3e1b54e215d03cba7
Siva Prakash <psivaprakash38@gmail.com>
2026-07-24 05:49:37 +0530

    added env variables

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /**
  * Read environment variables from file.
@@ -28,7 +31,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://opensource-demo.orangehrmlive.com/web/index.php/',
+    baseURL: process.env.BASE_URL,
      locale: 'en-US',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- tests/recruitment.spec.js
+++ tests/recruitment.spec.js
@@ -1,5 +1,4 @@
 import {test} from '../fixtures/modulesFixtures.js'
-import loginData from '../test_data/login.json'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/');
